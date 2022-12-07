a881c696d9743bab7841a437765f573db90a7db5
Minhyuk Jung <chmh0805@gmail.com>
2022-12-07 23:05:17 +0900

    Feat: Learn about variable

---
+++ app/src/variables.py
@@ -0,0 +1,13 @@
+a = 2
+b = 3
+c = a + b
+print(f'c : {c}')
+
+my_age = 28
+print(f'my_age is {my_age}')
+
+my_name = 'minhyuk'
+print(f'my name is {my_name}')
+
+dead = False
+print(f'dead is {dead}')
