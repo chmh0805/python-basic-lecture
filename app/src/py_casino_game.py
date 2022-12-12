b8f43bbbd6994eb28d8d7f2b4fd8cf5c8974e199
Minhyuk Jung <chmh0805@gmail.com>
2022-12-13 00:06:20 +0900

    Feat: upgrade Casino with while loop

--- app/src/py_casino_game.py
+++ app/src/py_casino_game.py
@@ -1,11 +1,16 @@
 from random import randint
 
-user_choice = int(input("Choose number."))
-pc_choice = randint(1, 50) # import this if have to use.
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100) # import this if have to use.
 
-if user_choice == pc_choice:
-    print("You won!")
-elif user_choice > pc_choice:
-    print("Lower! Computer chose", pc_choice)
-elif user_choice < pc_choice:
-    print("Higher! Computer chose", pc_choice)
+playing = True
+
+while playing:
+    user_choice = int(input("Choose number. (1-100)"))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
