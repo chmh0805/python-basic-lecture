ce7b58c841d2a11d55f10df61169207bbe6e3b27
Minhyuk Jung <chmh0805@gmail.com>
2022-12-13 00:35:03 +0900

    Feat: Learn about methods

---
+++ app/src/data_structure/methods.py
@@ -0,0 +1,9 @@
+# day_of_week =["Mon", "Tue", "Wed", "Thur", "Fri"]
+
+name = "nico"
+
+print(name.upper())
+print(name.capitalize())
+print(name.startswith("n"))
+print(name.endswith("o"))
+print(name.replace("o", "😎"))
