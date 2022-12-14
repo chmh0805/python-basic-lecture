0d48d3145fc2650645517be5c547624f5edc0278
Minhyuk Jung <chmh0805@gmail.com>
2022-12-15 00:42:15 +0900

    Feat: Learn about Lists

---
+++ app/src/data_structure/lists.py
@@ -0,0 +1,18 @@
+day_of_week =["Mon", "Tue", "Wed", "Thur", "Fri"]
+
+# print(day_of_week.count("Wed"))
+print(day_of_week)
+
+# day_of_week.clear()
+# print(day_of_week)
+
+# day_of_week.append("Sat")
+# print(day_of_week)
+
+# day_of_week.append("Sun")
+# print(day_of_week)
+
+# day_of_week.remove("Fri")
+# print(day_of_week)
+
+print(day_of_week[2])
