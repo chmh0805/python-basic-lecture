d472a1519a3ca6f7e7a6b0a0cbcf0c25cce8ac95
Minhyuk Jung <chmh0805@gmail.com>
2022-12-15 20:00:58 +0900

    Feat: Upgrade URL in for loop using format, if condition

--- app/src/data_structure/forLoop.py
+++ app/src/data_structure/forLoop.py
@@ -1,12 +1,15 @@
 websites = (
     "google.com",
     "airbnb.com",
-    "twitter.com",
-    "facebook.com"
+    "https://twitter.com",
+    "facebook.com",
+    "https://tiktok.com"
 )
 
 # print(websites[2])
 
 # Sequence can use for loop.
 for website in websites:
+    if website.startswith("https://") is False:
+        website = f"https://{website}"
     print("hello, Welcome to", website)
