8ba52a4a76813da62c4e968f3a69f731ff3ad335
Minhyuk Jung <chmh0805@gmail.com>
2022-12-15 19:55:20 +0900

    Feat: Learn about For Loop

---
+++ app/src/data_structure/forLoop.py
@@ -0,0 +1,12 @@
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com"
+)
+
+# print(websites[2])
+
+# Sequence can use for loop.
+for website in websites:
+    print("hello, Welcome to", website)
