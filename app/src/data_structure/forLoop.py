d13e8118a4ce7e378c77ad6179bc0d7d9f299e9d
Minhyuk Jung <chmh0805@gmail.com>
2022-12-15 20:16:48 +0900

    Feat: use requests

--- app/src/data_structure/forLoop.py
+++ app/src/data_structure/forLoop.py
@@ -1,3 +1,5 @@
+from requests import get
+
 websites = (
     "google.com",
     "airbnb.com",
@@ -6,10 +8,25 @@ websites = (
     "https://tiktok.com"
 )
 
+results = {}
+
 # print(websites[2])
 
 # Sequence can use for loop.
 for website in websites:
     if website.startswith("https://") is False:
         website = f"https://{website}"
-    print("hello, Welcome to", website)
+    response = get(website)
+    print(
+        response.status_code,
+        # response.content
+    )
+    if response.status_code == 200:
+        results[website] = "OK"
+        # print(f"{website} is OK. [{response.status_code}]")
+    else:
+        results[website] = "FAILED"
+        # print(f"{website} is not OK. [{response.status_code}]")
+    # print("hello, Welcome to", website)
+
+print(results)
