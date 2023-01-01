a4e16a5f874d5599e25d233eb4b6cd851479abbb
Minhyuk Jung <chmh0805@gmail.com>
2023-01-01 23:59:39 +0900

    Feat: Response with scraped data to Search Result Page, Beautify With Pico.css

--- app/src/extractor/app.py
+++ app/src/extractor/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from indeed.indeed_main import do_indeed_scrapper
+from wwr.wwr_main import do_weworkremotely_scrapper
 
 app = Flask("JobScrapper")
 
@@ -9,8 +11,12 @@ def home():
 
 
 @app.route("/search")
-def hello():
-    return render_template("search.html", keyword="aaaa")
+def search():
+    keyword = request.args.get("keyword")
+    indeed = do_indeed_scrapper(keyword, 10)
+    wwr = do_weworkremotely_scrapper(keyword)
+    job_results = indeed + wwr
+    return render_template("search.html", keyword=keyword, job_results=job_results)
 
 
 app.run(debug=True)
