8a1da416d606bf90b56d0a455c61adfc67cb61c2
Minhyuk Jung <chmh0805@gmail.com>
2023-01-02 01:13:36 +0900

    Fix: Broken Korean words bug in Excel

--- app/src/extractor/file.py
+++ app/src/extractor/file.py
@@ -1,10 +1,11 @@
 from os import path, mkdir
 
+
 def write_result_to_file(dirpath, filename, job_results):
     if path.isdir(dirpath) is False:
         mkdir(dirpath)
     file = open(path.join(dirpath, filename), "w", encoding="utf-8")
-    file.write("Position,Company,Location,URL\n")
+    file.write("\ufeffPosition,Company,Location,URL\n")
 
     for job_result in job_results:
         position = job_result['position'].replace(",", " ")
