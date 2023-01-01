21866b316043d57a566c03514dfe9a61f622c37c
Minhyuk Jung <chmh0805@gmail.com>
2023-01-02 00:09:29 +0900

    Feat: Make DB what works only in memory

--- app/src/extractor/app.py
+++ app/src/extractor/app.py
@@ -3,6 +3,7 @@ from indeed.indeed_main import do_indeed_scrapper
 from wwr.wwr_main import do_weworkremotely_scrapper
 
 app = Flask("JobScrapper")
+db = {}
 
 
 @app.route("/")
@@ -12,10 +13,14 @@ def home():
 
 @app.route("/search")
 def search():
-    keyword = request.args.get("keyword")
-    indeed = do_indeed_scrapper(keyword, 10)
-    wwr = do_weworkremotely_scrapper(keyword)
-    job_results = indeed + wwr
+    keyword = request.args.get("keyword").lower()
+    if keyword in db:
+        job_results = db[keyword]
+    else:
+        indeed = do_indeed_scrapper(keyword, 10)
+        wwr = do_weworkremotely_scrapper(keyword)
+        job_results = indeed + wwr
+        db[keyword] = job_results
     return render_template("search.html", keyword=keyword, job_results=job_results)
 
 
